9426054e48c59f2d25cb009f73ad6486722b3377
Rodrigo Munoz <romunozs@gmail.com>
2023-05-17 20:18:58 +0200

    Bootstrap code added to improve UI

--- src/app.js
+++ src/app.js
@@ -1,31 +1,57 @@
-/* eslint-disable */
-//import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/human-skull.png";
 
-window.onload = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+// window.onload = () => {
+//   let who = ["The dog", "My grandma", "His turtle", "My bird"];
+//   let action = ["ate", "peed", "crushed", "broke"];
+//   let what = ["my homework", "the keys", "the car"];
+//   let when = [
+//     "before the class",
+//     "right on time",
+//     "when I finished",
+//     "during my lunch",
+//     "while I was praying"
+//   ];
 
-  let master_excuse = [who, action, what, when];
-  let excuse = "";
-  for (let i = 0; i < 4; i++) {
-    let x = master_excuse[i].length;
-    let y = Math.floor(Math.random() * x);
-    let z = master_excuse[i][y];
-    excuse = excuse + z + " ";
-  }
+//   let master_excuse = [who, action, what, when];
+//   let excuse = "";
+//   for (let i = 0; i < 4; i++) {
+//     let x = master_excuse[i].length;
+//     let y = Math.floor(Math.random() * x);
+//     let z = master_excuse[i][y];
+//     excuse = excuse + z + " ";
+//   }
 
-  document.querySelector("#the-excuse").innerHTML = excuse;
-  console.log(excuse);
-};
+//   document.querySelector("#the-excuse").innerHTML = excuse;
+//   console.log(excuse);
+// };
+
+document.addEventListener("DOMContentLoaded", function() {
+  var runScriptButton = document.getElementById("runScriptButton");
+
+  runScriptButton.addEventListener("click", function() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let master_excuse = [who, action, what, when];
+    let excuse = "";
+    for (let i = 0; i < 4; i++) {
+      let x = master_excuse[i].length;
+      let y = Math.floor(Math.random() * x);
+      let z = master_excuse[i][y];
+      excuse = excuse + z + " ";
+    }
+
+    document.querySelector("#the-excuse").innerHTML = excuse;
+    console.log(excuse);
+  });
+});
